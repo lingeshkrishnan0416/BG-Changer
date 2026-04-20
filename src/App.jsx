95a9f1126ba18a6ba3219fb8f329761f6cf0334d
Lingesh Krishnan R <rsabari8991@gmail.com>
2026-04-21 02:41:35 +0530

    project closed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./APP.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("white");
+
+  function changeColor(color) {
+    setColor(color);
+  }
 
   return (
-    <>
-      
-    </>
-  )
+    <div
+      className="flex justify-center min-h-screen"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed bottom-10  flex justify-center p-2 bg-black">
+        <div className="flex flex-wrap justify-center gap-4 bg-black/50 backdrop-blur-md rounded-xl p-4 shadow-lg">
+          <button
+            onClick={() => changeColor("red")}
+            className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 transition-all duration-200"
+          >
+            Press
+          </button>
+          <button
+            onClick={() => changeColor("blue")}
+            className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 transition-all duration-200"
+          >
+            Press
+          </button>
+          <button
+            onClick={() => changeColor("green")}
+            className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 transition-all duration-200"
+          >
+            Press
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
